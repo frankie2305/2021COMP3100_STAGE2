80f78bc892879d646ba277d9f3b3cde1fadb4b3e
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-05-03 10:00:00 +0700

    Added Parser.java

---
+++ src/pre-compiled/Parser.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
+public class Parser {
+    public static Job parseJob(String[] specs) {
+        Job job = null;
+        try {
+            job = new Job(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Integer.parseInt(specs[3]),
+                    Integer.parseInt(specs[4]), Integer.parseInt(specs[5]), Integer.parseInt(specs[6]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return job;
+    }
+
+    public static Server parseServer(String[] specs) {
+        Server server = null;
+        try {
+            server = new Server(specs[0], Integer.parseInt(specs[1]), specs[2], Integer.parseInt(specs[3]),
+                    Integer.parseInt(specs[4]), Integer.parseInt(specs[5]), Integer.parseInt(specs[6]),
+                    Integer.parseInt(specs[7]), Integer.parseInt(specs[8]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return server;
+    }
+
+    public static ArrayList<Server> parseXMLServers() {
+        ArrayList<Server> servers = new ArrayList<Server>();
+        try {
+            File dsSystem = new File("ds-system.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(dsSystem);
+            document.getDocumentElement().normalize();
+            NodeList xmlServers = document.getElementsByTagName("server");
+            for (int i = 0; i < xmlServers.getLength(); i++) {
+                Node node = xmlServers.item(i);
+                Element specs = (Element) node;
+                String type = specs.getAttribute("type");
+                int limit = Integer.parseInt(specs.getAttribute("limit"));
+                int bootupTime = Integer.parseInt(specs.getAttribute("bootupTime"));
+                float hourlyRate = Float.parseFloat(specs.getAttribute("hourlyRate"));
+                int core = Integer.parseInt(specs.getAttribute("coreCount"));
+                int memory = Integer.parseInt(specs.getAttribute("memory"));
+                int disk = Integer.parseInt(specs.getAttribute("disk"));
+                servers.add(new Server(type, limit, bootupTime, hourlyRate, core, memory, disk));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return servers;
+    }
+}
